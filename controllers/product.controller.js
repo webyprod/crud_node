33783eedb73ecd32d67637e011a3d3c318a1b33b
mccloud <mk.alex1989@gmail.com>
2019-10-26 00:29:15 +0200

    finish product API read create put delete

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -17,11 +17,48 @@ router.get("/", (req, res) => {
 })
 
 router.get("/:id", (req, res) => {
-    connection.query("Select * from product where id =:id", (err,records,fields)=>{
+    connection.query("Select * from product where id="+req.params.id, (err,records,fields)=>{
         if(err){
             console.error("Error while fetching data");
         }else{
             res.send(records);
+        }
+    })
+})
+
+router.post("/", (req, res) => {
+    var id = req.body.id;
+    var name = req.body.name;
+    var description = req.body.description;
+    var price = req.body.price;
+    connection.query("insert into product values ("+id+",'"+name+"','"+description+"',"+price+")", (err,records)=>{
+        if(err){
+            console.error("Error while inserting data : "+err);
+        }else{
+            res.send({insert:"success"});
+        }
+    })
+})
+
+router.put("/", (req, res) => {
+    var id = req.body.id;
+    var name = req.body.name;
+    var price = req.body.price;
+    connection.query("update product set name='"+name+"',price='"+price+"' where id="+id, (err,records)=>{
+        if(err){
+            console.error("Error while inserting data : "+err);
+        }else{
+            res.send({insert:"success"});
+        }
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    connection.query("Delete from product where id="+req.params.id, (err,records)=>{
+        if(err){
+            console.error("Error while deleting data");
+        }else{
+            res.send({delete:"delete success"});
         }
     })
 })
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require("express");
+
+var bodyparser = require("body-parser");
+
 var app = express();
+
 var productAPI = require("./controllers/product.controller");
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded())
 
 app.use("/api/products", productAPI);
 
