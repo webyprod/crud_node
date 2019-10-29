953592c34d36b816d6dbf75540eafcb167941e50
mccloud <mk.alex1989@gmail.com>
2019-10-29 01:07:53 +0100

    modif server

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ var customerAPI = require("./controllers/customer.controller");
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+  });
+
 app.use("/api/products", productAPI);
 app.use("/api/customers", customerAPI);
 
